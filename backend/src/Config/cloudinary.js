10ff37bfe9c8c3291bf557600d5d7fbe153f76f0
kinza <kinzashahzadi7181@gmail.com>
2026-03-21 04:43:14 +0500

    feat: project folder structure and server scaffold with cloudinary

---
+++ backend/src/Config/cloudinary.js
@@ -0,0 +1,14 @@
+import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+
+dotenv.config();
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+console.log("Cloudinary Name:", process.env.CLOUD_NAME);
+console.log("Cloudinary Key:", process.env.CLOUD_API_KEY);
+console.log("Cloudinary Secret:", process.env.CLOUD_API_SECRET);
+
+export default cloudinary;
